12a132ca716ea04ef5c3e5d9c6b8996e7d11cb6e
sahithi0204 <113923373+sahithi0204@users.noreply.github.com>
2025-02-14 08:41:47 +0530

    Create Q-1.R

---
+++ Q-1.R
@@ -0,0 +1,5 @@
+name <- readline(prompt = "Please enter your name: ")
+age <- readline(prompt = "Please enter your age: ")
+cat("Your name is:", name, "\n")
+cat("Your age is:", age, "\n")
+cat("You are using R version:", R.version$version.string, "\n")
