2647b254eb63d6cbff9da9f8919f58e672d37c87
sahithi0204 <113923373+sahithi0204@users.noreply.github.com>
2025-02-17 11:50:01 +0530

    Create Q-16.R

---
+++ Q-16.R
@@ -0,0 +1,6 @@
+mat1 <- matrix(1:20, nrow=5, ncol=4, byrow=TRUE)
+print(mat1)
+mat2 <- matrix(1:9, nrow=3, ncol=3, byrow=TRUE, dimnames=list(c("A", "B", "C"), c("X", "Y", "Z")))
+print(mat2)
+mat3 <- matrix(1:4, nrow=2, ncol=2, byrow=FALSE, dimnames=list(c("Row1", "Row2"), c("Col1", "Col2")))
+print(mat3)
