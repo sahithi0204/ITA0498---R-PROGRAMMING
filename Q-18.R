db1500ca735f841df7506a8cf7d0b4ac16fc4a46
sahithi0204 <113923373+sahithi0204@users.noreply.github.com>
2025-02-17 11:56:03 +0530

    Create Q-18.R

---
+++ Q-18.R
@@ -0,0 +1,5 @@
+vector1 <- c(1, 2, 3, 4, 5, 6)
+vector2 <- c(7, 8, 9, 10, 11, 12)
+# Create an array with 3 rows, 3 columns, and 2 tables
+arr <- array(c(vector1, vector2), dim = c(3, 3, 2))
+print(arr)
