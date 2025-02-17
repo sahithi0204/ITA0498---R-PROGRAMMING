8dd22b44bc31c0288194b670404030200ea30bb0
sahithi0204 <113923373+sahithi0204@users.noreply.github.com>
2025-02-17 11:44:28 +0530

    Create Q-15.R

---
+++ Q-15.R
@@ -0,0 +1,9 @@
+num_vec <- c(1, 2, 3)       # Numeric vector
+char_vec <- c("A", "B", "C") # Character vector
+log_vec <- c(TRUE, FALSE, TRUE) # Logical vector
+print(num_vec)
+print(typeof(num_vec))
+print(char_vec)
+print(typeof(char_vec))
+print(log_vec)
+print(typeof(log_vec))
