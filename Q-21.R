3b629165bf74663c2d2943400388175d8ca40203
sahithi0204 <113923373+sahithi0204@users.noreply.github.com>
2025-02-17 12:03:23 +0530

    Create Q-21.R

---
+++ Q-21.R
@@ -0,0 +1,5 @@
+vector1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+vector2 <- c(10, 11, 12, 13, 14, 15, 16, 17, 18)
+arr <- array(c(vector1, vector2), dim = c(3, 3, 2))
+print(arr[2, , 2])
+print(arr[3, 3, 1])
